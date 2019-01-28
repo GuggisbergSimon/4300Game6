c9ed1642bdbe1689dda2b0d017a3529290d2ef80
Simon <slyguggi@gmail.com>
2019-01-28 12:47:42 +0100

    improved ink behaviour and beacons

--- Assets/Scripts/Beacon.cs
+++ Assets/Scripts/Beacon.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class Beacon : MonoBehaviour
 {
-	[SerializeField]private GameObject ink;
-	private bool _isActivated = false;
-	
+	[SerializeField] private GameObject inkPrefab = null;
+	[SerializeField] private float minRotationConstantSpeed = 1.0f;
+	[SerializeField] private float maxRotationConstantSpeed = 2.0f;
+	private bool _isInked = false;
+	private float _rotationConstantSpeed;
+
+	private void Start()
+	{
+		_rotationConstantSpeed = Random.Range(minRotationConstantSpeed, maxRotationConstantSpeed);
+	}
+
+	private void Update()
+	{
+		//if (!_isInked)
+		{
+			transform.Rotate(0, 0, _rotationConstantSpeed * Time.deltaTime);
+		}
+	}
+
 	private void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.CompareTag("Player") && !_isActivated)
+		if (!_isInked && other.CompareTag("Player"))
 		{
 			GameManager.Instance.Player.BeaconActivated(gameObject);
-			_isActivated = true;
-			gameObject.SetActive(false);
-			Instantiate(ink, transform.position, Quaternion.identity);
+			_isInked = true;
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				transform.GetChild(i).gameObject.SetActive(false);
+			}
+
+			Instantiate(inkPrefab, transform.position, Quaternion.Euler(0, 0, Random.Range(0, 360)), transform);
 		}
 	}
 }
--- Assets/Scripts/InkBehavior.cs
+++ Assets/Scripts/InkBehavior.cs
@@ -5,22 +5,58 @@ using UnityEngine;
 
 public class InkBehavior : MonoBehaviour
 {
-	[SerializeField] private float time;
-	[SerializeField] private Vector2 maxScale;
+	[SerializeField] private float minTimeGrowing = 1.0f;
+	[SerializeField] private float maxTimeGrowing = 2.0f;
+	[SerializeField] private float minScaleAfterGrowing = 3.0f;
+	[SerializeField] private float maxScaleAfterGrowing = 5.0f;
+	[SerializeField] private float minRotationConstantSpeed = 1.0f;
+	[SerializeField] private float maxRotationConstantSpeed = 2.0f;
+	[SerializeField] private int minInkSpawned = 2;
+	[SerializeField] private int maxInkSpawned = 5;
+	[SerializeField] private GameObject inkPrefab = null;
+	[SerializeField] private Color[] colors = null;
+	[SerializeField] private Sprite[] sprites = null;
+	private float _timeGrowing;
+	private float _scaleAfterGrowing;
+	private float _rotationConstantSpeed;
+	private int _numberInkSpawned;
+	private SpriteRenderer _mySprite;
 
 	private void Start()
 	{
-		StartCoroutine(InkExplosion(time));
+		_mySprite = GetComponentInChildren<SpriteRenderer>();
+		_mySprite.sprite = sprites[Random.Range(0, sprites.Length)];
+		_mySprite.color = colors[Random.Range(0, colors.Length)];
+		_timeGrowing = Random.Range(minTimeGrowing, maxTimeGrowing);
+		_scaleAfterGrowing = Random.Range(minScaleAfterGrowing, maxScaleAfterGrowing);
+		_rotationConstantSpeed = Random.Range(minRotationConstantSpeed, maxRotationConstantSpeed);
+		_numberInkSpawned = Random.Range(minInkSpawned, maxInkSpawned);
+		StartCoroutine(InkGrowing(_timeGrowing));
 	}
 
-	private IEnumerator InkExplosion(float time)
+	private void Update()
+	{
+		transform.Rotate(0, 0, _rotationConstantSpeed * Time.deltaTime);
+	}
+
+	private IEnumerator InkGrowing(float time)
 	{
 		float timer = 0.0f;
-		while (true)
+		while (timer < time)
 		{
-			transform.localScale = Vector3.Lerp(Vector2.zero, maxScale, timer / time);
+			transform.localScale = Vector3.Lerp(Vector2.zero,
+				Vector2.one * _scaleAfterGrowing, timer / time);
 			timer += Time.deltaTime;
 			yield return null;
+		}
+
+		for (int i = 0; i < _numberInkSpawned; i++)
+		{
+			float width = 3.0f;
+			GameObject ink = Instantiate(inkPrefab,
+				transform.position + width * _scaleAfterGrowing * new Vector3(Random.Range(-1,1), Random.Range(-1,1), 0),
+				Quaternion.Euler(0, 0, Random.Range(0, 360)), transform.parent);
+			ink.GetComponentInChildren<SpriteRenderer>().sortingOrder = _mySprite.sortingOrder + i + 1;
 		}
 	}
 }
