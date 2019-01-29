cd96ad34ae81c431135345b7300148d20d95f226
Simon <slyguggi@gmail.com>
2019-01-29 01:36:49 +0100

    finalized beaconfinder

--- Assets/Scripts/BeaconFinder.cs
+++ Assets/Scripts/BeaconFinder.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class BeaconFinder : MonoBehaviour
 {
 	[SerializeField] private float changeTime = 1.0f;
-	[SerializeField] private float percentDistanceOnScreen = 0.1f;
 	private Vector2 _closestBeaconPosition;
+	private Vector2 _currentPos;
 	private Coroutine _movingCoroutine;
 
 	private void Start()
@@ -17,6 +17,9 @@ public class BeaconFinder : MonoBehaviour
 	private void Update()
 	{
 		Scan();
+		Debug.Log(Vector3.Angle(Vector2.up, _currentPos - (Vector2) GameManager.Instance.Player.transform.position));
+		//transform.position = _currentPos;
+		transform.parent.up = (Vector3) _currentPos - GameManager.Instance.Player.transform.position;
 	}
 
 	private void Scan()
@@ -24,7 +27,7 @@ public class BeaconFinder : MonoBehaviour
 		foreach (var beacon in GameManager.Instance.BeaconsLeft)
 		{
 			if ((GameManager.Instance.Player.transform.position - (Vector3) _closestBeaconPosition).magnitude >
-				(GameManager.Instance.Player.transform.position - beacon.transform.position).magnitude)
+			    (GameManager.Instance.Player.transform.position - beacon.transform.position).magnitude)
 			{
 				_closestBeaconPosition = beacon.transform.position;
 				if (_movingCoroutine != null)
@@ -49,7 +52,7 @@ public class BeaconFinder : MonoBehaviour
 		float timer = 0.0f;
 		while (timer < time)
 		{
-			transform.position = Vector2.Lerp(initPos, _closestBeaconPosition, timer / time);
+			_currentPos = Vector2.Lerp(initPos, _closestBeaconPosition, timer / time);
 			timer += Time.deltaTime;
 			yield return null;
 		}
